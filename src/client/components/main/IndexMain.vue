ef788880c32292694fecba305f80f80d0761f870
AveryOn <lnkonion2@gmail.com>
2026-06-08 21:52:47 +0400

    [2026-06-08-1780941167]

--- src/client/components/main/IndexMain.vue
+++ src/client/components/main/IndexMain.vue
@@ -1,8 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { plural } from '~/client/plugins/i18n'
+
+const { t: $t } = useI18n()
+
+const count = 101
+</script>
 
 <template>
   <div class="main">
     MAIN
+    <div>
+      <p>{{ $t('Hello World!') }}</p>
+      <p>{{ $t('Example 123') }}</p>
+      <p>{{ plural('Project', count) }}</p>
+      <p>{{ plural('You have {count} project', count) }}</p>
+    </div>
   </div>
 </template>
 
