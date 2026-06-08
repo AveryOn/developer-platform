b5c2ffc98480b76f9bb796b882cf9112a7ac2d1d
AveryOn <lnkonion2@gmail.com>
2026-06-08 17:56:04 +0400

    [2026-06-08-1780926964]

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,6 +1,7 @@
 import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
+import { serverEnv } from '~/config/env'
 
-const sqlite = new Database('./data.sqlite')
+const sqlite = new Database(serverEnv.DATABASE_URL)
 
 export const db = drizzle(sqlite)
