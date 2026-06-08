a72b0c4d5a4430770ad95bc7b59a3e07a99b2acb
AveryOn <lnkonion2@gmail.com>
2026-06-08 20:25:00 +0400

    [2026-06-08-1780935900]

---
+++ src/client/layouts/BaseLayout.client.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="base-layout">
+        <header class="base-layout__header">
+            <slot name="header" />
+        </header>
+
+        <main class="base-layout__main">
+            <slot name="main" />
+        </main>
+
+        <footer class="base-layout__footer">
+            <slot name="footer" />
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+.base-layout {
+    min-height: 100dvh;
+    display: flex;
+    flex-direction: column;
+}
+
+.base-layout__header {
+    flex: 0 0 auto;
+}
+
+.base-layout__main {
+    flex: 1 1 auto;
+}
+
+.base-layout__footer {
+    flex: 0 0 auto;
+}
+</style>
