8cd81e4dd3a2cb785249d9ddf7fa844846c5345c
AveryOn <lnkonion2@gmail.com>
2026-06-08 17:27:42 +0400

    [2026-06-08-1780925262]

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import js from '@eslint/js'
+import astro from 'eslint-plugin-astro'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
+import vue from 'eslint-plugin-vue'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  ...astro.configs.recommended,
+
+  {
+    ignores: [
+      'dist',
+      '.astro',
+      'node_modules',
+      'drizzle',
+      '*.config.js',
+      '*.config.mjs'
+    ]
+  },
+
+  {
+    files: ['**/*.{ts,tsx,vue,astro}'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: ['.astro', '.vue']
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'vue/multi-word-component-names': 'off'
+    }
+  },
+
+  eslintConfigPrettier
+)
