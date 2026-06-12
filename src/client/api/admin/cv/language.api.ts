47d954bc8fae7d20a357e110cb9608e211a22ab6
AveryOn <lnkonion2@gmail.com>
2026-06-12 16:20:23 +0400

    [2026-06-12-1781266823]

--- src/client/api/admin/cv/language.api.ts
+++ src/client/api/admin/cv/language.api.ts
@@ -1,10 +1,13 @@
 import { httpClient } from '~/client/api/http-client'
 import type { Language } from '~/shared/dto/cv/language.dto'
+import { AppRoutes } from '~/shared/router'
+
+const routes = AppRoutes.api.admin
 
 export const CvLanguageApi = {
   async getList() {
     const result = await httpClient.get<{ data: Language[] }>(
-      `/cv/languages/`,
+      routes.CvLanguages,
     )
     return result.data
   },
