d6416e69c75218ca60e658bb5d30edcf6d9804d9
AveryOn <lnkonion2@gmail.com>
2026-06-12 16:22:26 +0400

    [2026-06-12-1781266946]

--- src/client/api/admin/cv/skill.api.ts
+++ src/client/api/admin/cv/skill.api.ts
@@ -1,10 +1,13 @@
 import { httpClient } from '~/client/api/http-client'
 import type { Skill } from '~/shared/dto/cv/skill.dto'
+import { AppRoutes } from '~/shared/router'
+
+const routes = AppRoutes.api.admin
 
 export const CvSkillApi = {
   async getList() {
     const result = await httpClient.get<{ data: Skill[] }>(
-      `/cv/skills`,
+      routes.CvSkills,
     )
     return result.data
   },
