d433196e26f4b409a430bed00418f2f425b2d414
AveryOn <lnkonion2@gmail.com>
2026-06-12 16:20:05 +0400

    [2026-06-12-1781266805]

--- src/client/api/admin/cv/experience.api.ts
+++ src/client/api/admin/cv/experience.api.ts
@@ -1,10 +1,13 @@
 import { httpClient } from '~/client/api/http-client'
 import type { Experience } from '~/shared/dto/cv/experience.dto'
+import { AppRoutes } from '~/shared/router'
+
+const routes = AppRoutes.api.admin
 
 export const CvExperienceApi = {
   async getList() {
     const result = await httpClient.get<{ data: Experience[] }>(
-      `/cv/experiences/`,
+      routes.CvExperiences,
     )
     return result.data
   },
