8903061c88c6a151a301e4a6dd59d3b1a443b058
AveryOn <lnkonion2@gmail.com>
2026-06-12 11:40:49 +0400

    [2026-06-12-1781250049]

--- src/client/api/admin/cv/links.api.ts
+++ src/client/api/admin/cv/links.api.ts
@@ -2,7 +2,7 @@ import { httpClient } from '~/client/api/http-client'
 import type { Profile } from '~/shared/dto/cv/profile.dto'
 
 export const CvLinksApi = {
-  async getListByProfileId(profileId: string) {
+  async getListByProfileId(profileId?: string) {
     const result = await httpClient.get<{ data: Profile[] }>(
       `/cv/links/`,
       {
