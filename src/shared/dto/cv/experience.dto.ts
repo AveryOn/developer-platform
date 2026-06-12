c8bb94988c7fcc9b2f631a402069b42dd7959e83
AveryOn <lnkonion2@gmail.com>
2026-06-12 12:03:10 +0400

    [2026-06-12-1781251390]

--- src/shared/dto/cv/experience.dto.ts
+++ src/shared/dto/cv/experience.dto.ts
@@ -1,3 +1,4 @@
 import type { cvExperienceTable } from '~/server/database/schema'
 
 export type Experience = typeof cvExperienceTable.$inferSelect
+export type ExperienceInput = typeof cvExperienceTable.$inferInsert
