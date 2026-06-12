4d43ace21a966eb23464a8d92d28fae790c8b189
AveryOn <lnkonion2@gmail.com>
2026-06-12 13:34:29 +0400

    [2026-06-12-1781256869]

--- src/shared/router/api.routes.ts
+++ src/shared/router/api.routes.ts
@@ -1,8 +1,13 @@
 export const apiRoutes = {
-  Health: '/api/health',
-  Projects: '/api/projects',
-  Articles: '/api/articles',
-  CvProfiles: '/api/cv/profiles',
-  CvProfilesActive: '/api/cv/profiles/active',
-  cvProfileByUuid: (uuid: string) => `/api/cv/profiles/${uuid}`,
-}
+  client: {
+    Health: '/api/health',
+    Projects: '/api/projects',
+    Articles: '/api/articles',
+  },
+
+  admin: {
+    CvProfiles: '/api/cv/profiles',
+    CvProfilesActive: '/api/cv/profiles/active',
+    cvProfileByUuid: (uuid: string) => `/api/cv/profiles/${uuid}`,
+  },
+} as const
