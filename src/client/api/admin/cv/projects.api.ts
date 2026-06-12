fd335c4c0ddca7cd39ee2168e4c150b03afae05f
AveryOn <lnkonion2@gmail.com>
2026-06-12 11:25:03 +0400

    [2026-06-12-1781249103]

--- src/client/api/admin/cv/projects.api.ts
+++ src/client/api/admin/cv/projects.api.ts
@@ -1,8 +1,8 @@
 import { httpClient } from '~/client/api/http-client'
 import type { Experience } from '~/shared/dto/cv/experience.dto'
 
-export const CvLinksApi = {
-  async getList() {
+export const CvProjectsApi = {
+  async getList(profileId: string) {
     const result = await httpClient.get<{ data: Experience[] }>(`/cv/experience/`)
     return result.data
   },
