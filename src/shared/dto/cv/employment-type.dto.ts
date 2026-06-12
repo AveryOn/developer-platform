f7a7adbc2824df274b73eabf388e35011cad2fc0
AveryOn <lnkonion2@gmail.com>
2026-06-12 12:04:06 +0400

    [2026-06-12-1781251446]

---
+++ src/shared/dto/cv/employment-type.dto.ts
@@ -0,0 +1,4 @@
+import type { cvEmploymentTypeTable } from '~/server/database/schema'
+
+export type EmploymentType = typeof cvEmploymentTypeTable.$inferSelect
+export type EmploymentTypeInput = typeof cvEmploymentTypeTable.$inferInsert
