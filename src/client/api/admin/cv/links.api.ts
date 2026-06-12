b5c6ea949b56976f72b6a49de9682ec3103c839c
AveryOn <lnkonion2@gmail.com>
2026-06-12 11:40:36 +0400

    [2026-06-12-1781250036]

--- src/client/api/admin/cv/links.api.ts
+++ src/client/api/admin/cv/links.api.ts
@@ -13,28 +13,4 @@ export const CvLinksApi = {
     )
     return result.data
   },
-
-  // getById(uuid: string) {
-  //   return httpClient.get<Profile[]>(`/cv/profiles/${uuid}`)
-  // },
-
-  // getActive() {
-  //   return httpClient.get<Profile[]>('/cv/profiles', {
-  //     params: {
-  //       isActive: true,
-  //     },
-  //   })
-  // },
-
-  // create(data: CreateCvProfileDto) {
-  //   return httpClient.post<Profile[]>(`/cv/profiles`, {
-  //     data,
-  //   })
-  // },
-
-  // update(uuid: string, data: UpdateCvProfileDto) {
-  //   return httpClient.patch<Profile[]>(`/cv/profiles/${uuid}`, {
-  //     data,
-  //   })
-  // },
 }
