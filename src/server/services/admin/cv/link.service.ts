d5ac37ff65d8b81823ca6d218266ab4bd89da585
AveryOn <lnkonion2@gmail.com>
2026-07-09 13:27:21 +0400

    [2026-07-09-1783589241]

--- src/server/services/admin/cv/link.service.ts
+++ src/server/services/admin/cv/link.service.ts
@@ -1,7 +1,7 @@
 import { cvProfileLinkTable } from '~/server/database/schema'
 import { db } from '~/server/database/client'
 import type { CreateCvLinkDto, CreateLinkResponse, Link } from '~/shared/dto/cv/link.dto'
-import { and, eq } from 'drizzle-orm'
+import { and, eq, sql } from 'drizzle-orm'
 import { dateISO } from '~/shared/utils/datetime'
 import type { Logger } from '~/shared/logger/logger.client'
 // import { dateISO } from '~/shared/utils/datetime'
@@ -61,44 +61,44 @@ export const CvLinkService = {
       })
       logger?.info('Reorder links:: COMPLETE', { shiftedLinks })
 
+
       // Фиксирование индексов порядка для всех остальных link в профиле
-      await Promise.all(shiftedLinks.map(async (link) => {
-        const [data] = await tx
+      logger?.info('Reorder links COMMIT:: PENDING')
+      for (const link of linksOnProfile) {
+        await tx
           .update(cvProfileLinkTable)
           .set({
             order: link.order
           })
-          .where(
-            eq(cvProfileLinkTable.id, link.id)
-          )
-          .returning({
-            id: cvProfileLinkTable.id,
-            order: cvProfileLinkTable.order,
-          })
-          .limit(1)
-        return data
-      }))
+          .where(eq(cvProfileLinkTable.id, link.id))
+      }
+      logger?.info('Reorder links COMMIT:: COMPLETE')
 
 
-      // const [newLink] = await tx
-      //   .insert(cvProfileLinkTable)
-      //   .values({
-      //     label: dto.label,
-      //     profileId: dto.profileId,
-      //     type: dto.type,
-      //     url: dto.url,
-      //     order: 0,
-      //     isVisible: true,
-      //     updatedAt: now,
-      //     createdAt: now,
-      //   })
-      //   .returning()
+      logger?.info('Create new link:: PENDING')
+      const [newLink] = await tx
+        .insert(cvProfileLinkTable)
+        .values({
+          label: dto.label,
+          profileId: dto.profileId,
+          type: dto.type,
+          url: dto.url,
+          order: 1,
+          isVisible: true,
+          updatedAt: now,
+          createdAt: now,
+        })
+        .returning()
 
-      // return {
-      //   newLink,
-      //   shiftedLinks: shiftedLinks
-      // }
-      throw ''
+      logger?.info('Create new link:: COMPLETE', {
+        newLinkId: newLink.id,
+        newLinkLabel: newLink.label,
+      })
+
+      return {
+        newLink,
+        shiftedLinks: shiftedLinks
+      }
     })
   },
 }
