88fba0b88eaef50c740a61eb95d0ccbd371e7e16
AveryOn <lnkonion2@gmail.com>
2026-07-11 13:07:10 +0400

    [2026-07-11-1783760830] add

--- src/client/api/admin/cv/experience.api.ts
+++ src/client/api/admin/cv/experience.api.ts
@@ -1,13 +1,17 @@
 import { httpClient } from '~/client/api/http-client'
+import { _ } from '~/shared/const'
 import type { Experience } from '~/shared/dto/cv/experience.dto'
 import { AppRoutes } from '~/shared/router'
 
 const routes = AppRoutes.api.admin
 
 export const CvExperienceApi = {
-  async getList() {
+  async getListByProfileId(profileId?: string) {
     const result = await httpClient.get<{ data: Experience[] }>(
-      routes.CvExperiences,
+      routes.CvLinks,
+      {
+        params: profileId ? { profileId } : _,
+      },
     )
     return result.data
   },
--- src/client/components/admin/cv/links/CvLinksView.vue
+++ src/client/components/admin/cv/links/CvLinksView.vue
@@ -1,287 +1,38 @@
 <script setup lang="ts">
 import { mdiChevronDownBoxOutline, mdiChevronUpBoxOutline, mdiHandOkay, mdiPen, mdiUndo } from '@mdi/js'
-import { computed, onBeforeMount, ref } from 'vue'
-import { CvLinksApi } from '~/client/api/admin/cv/links.api'
 import Icon from '~/client/components/common/Icon.vue'
 import SelectInputUI from '~/client/components/shared/SelectInputUI.vue'
-import { useKeyboard } from '~/client/composables/useKeyboard'
 import { _ } from '~/shared/const'
-import type { Link, PatchCvLinkDto } from '~/shared/dto/cv/link.dto'
 import { SocialNetworks } from '~/shared/types'
-import type { LinkInput } from '~/shared/dto/cv/link.dto';
 import InputUI from '~/client/components/shared/InputUI.vue'
 import CheckboxUI from '~/client/components/shared/CheckboxUI.vue'
 import ButtonBaseUI from '~/client/components/shared/ButtonBaseUI.vue'
-import { AppRoutes } from '~/shared/router'
-import { useProfiles } from '~/client/composables/useProfiles'
-import { useToast } from '~/client/composables/useToast'
+import { useCvLinksEditor } from '~/client/composables/useCvLinksEditor'
 
-useKeyboard({
-  esc: resetSelection,
-})
-const toast = useToast()
 const {
   profiles,
-  selectedProfileId
-} = useProfiles({ setFirstAsSelect: true })
+  selectedProfileId,
+  isSaveReorderLoading,
+  entitiesByProfileId: linksByProfileId,
+  selectedEntity: selectedLink,
 
-interface LinkOrderItem {
-  id: string
-  order: number
-  label?: string
-}
+  editFormData: editLinkFormData,
 
-const links = ref<Link[]>([])
-const linksOrder = ref<LinkOrderItem[]>([])
-const isSaveReorderLoading = ref(false)
-const isSubmitFormChangesLoading = ref(false)
+  entitiesAreReordered: linksAreReordered,
+  someChange,
 
-/** true - если порядок ссылок изменен */
-const linksAreReordered = computed(() => {
-  for (let i = 0; i < links.value.length; i++) {
-    const link = links.value[i];
-    if (linksOrder.value[i].order !== link.order) {
-      return true
-    }
-  }
-  return false
-})
+  selectEntity: selectLink,
+  moveEntity: moveLink,
+  loadEntities: uploadLinks,
 
-const linksByProfileId = computed(() => {
-  if (!selectedProfileId.value) return links.value
-  return links.value.filter(v => (v.profileId === selectedProfileId.value))
-})
-const selectedLink = ref<Link | null>(null)
-
-type LinkEditData = Partial<Record<keyof LinkInput, {
-  oldValue: LinkInput[keyof LinkInput]
-  newValue: LinkInput[keyof LinkInput]
-  focused: boolean,
-  loading: boolean,
-}>>
-type LinkEditBody = Partial<Record<keyof LinkInput, LinkInput[keyof LinkInput]>>
-
-const editLinkFormData = ref<LinkEditData>({
-  isVisible: { newValue: _, oldValue: _, focused: false, loading: false },
-  label: { newValue: _, oldValue: _, focused: false, loading: false },
-  order: { newValue: _, oldValue: _, focused: false, loading: false },
-  profileId: { newValue: _, oldValue: _, focused: false, loading: false },
-  type: { newValue: _, oldValue: _, focused: false, loading: false },
-  url: { newValue: _, oldValue: _, focused: false, loading: false },
-})
-
-function selectLink(link: Link) {
-  selectedLink.value = link
-  for (const key of Object.keys(link)) {
-    const k = key as keyof typeof link
-    editLinkFormData.value[k] = {
-      newValue: link[k],
-      oldValue: link[k],
-      focused: false,
-      loading: false
-    }
-  }
-}
-
-function resetSelection() {
-  selectedLink.value = null
-  Object.values(editLinkFormData.value).forEach((v) => {
-    v.newValue = _
-    v.oldValue = _
-    v.focused = false
-    v.loading = false
-  })
-  resetChangesOrder()
-}
-
-/** Обновляет исходный массив ссылок после изменений */
-function refreshSource(body: LinkEditBody) {
-  links.value = links.value.map((l) => {
-    if (l.id === selectedLink.value?.id) {
-      return { ...l, ...body as Link }
-    }
-    return l
-  })
-}
-
-/** Отменить изменения в поле */
-function undoChanges(field: keyof LinkInput) {
-  editLinkFormData.value[field]!.newValue = editLinkFormData.value[field]?.oldValue
-  resetFocus(field)
-}
-
-/** Проверка есть ли изменения в поле */
-function hasChanges(field: keyof LinkInput) {
-  return editLinkFormData.value[field]!.newValue !== editLinkFormData.value[field]?.oldValue
-}
-
-/** Сбросить фокус на поле */
-function resetFocus(field: keyof LinkInput) {
-  editLinkFormData.value[field]!.focused = false
-}
-
-/** Есть ли хотя бы одно изменение сущности link */
-const someChange = computed(() => {
-  return Object
-    .values(editLinkFormData.value)
-    .some((v) => v.newValue !== v.oldValue)
-})
-
-/** Подтвердить изменение поля */
-async function confirmUpdateField(field: keyof LinkInput) {
-  try {
-    if (!selectedLink.value) return
-    if (!hasChanges(field)) return
-    editLinkFormData.value[field]!.loading = true
-
-    const body = { [field]: editLinkFormData.value[field]?.newValue }
-    const result = await CvLinksApi.patch(selectedLink.value.id, body)
-    if (!result) throw _;
-
-    //  Зафиксировать изменение в исходном массиве
-    refreshSource(body)
-    resetFocus(field)
-    resetSelection()
-
-    toast.success('Ссылка изменена', { duration: 3000, title: 'Success!' })
-  }
-  catch (err) {
-    console.error(err)
-    toast.error('Произошла ошибка при обновлении поля', {
-      duration: 3000,
-      title: 'Ошибка',
-    })
-  }
-  finally {
-    editLinkFormData.value[field]!.loading = false
-  }
-}
-
-function resetFormChanges() {
-  for (const k of Object.keys(editLinkFormData.value)) {
-    undoChanges(k as keyof LinkInput)
-  }
-}
-
-async function submitFormChanges() {
-  try {
-    isSubmitFormChangesLoading.value = true
-
-    if (!selectedLink.value) return
-    // Если изменений форме ссылки нет, тогда выход
-    if (!someChange.value) return
-
-    // Собираем объект измененных полей ссылки
-    const body: LinkEditBody = {}
-    for (const [k, v] of Object.entries(editLinkFormData.value)) {
-      if (hasChanges(k as keyof LinkInput)) {
-        body[k as keyof LinkInput] = v.newValue
-      }
-    }
-    const result = await CvLinksApi.patch(selectedLink.value?.id, body as PatchCvLinkDto)
-    if (!result) throw _
-
-    //  Зафиксировать изменение в исходном массиве
-    refreshSource(body)
-    resetSelection()
-    toast.success('Ссылка изменена', { duration: 3000, title: 'Success!' })
-  } catch (err) {
-    console.error(err)
-    toast.error('Произошла ошибка при изменении данных ссылки', {
-      duration: 3000,
-      title: 'Ошибка',
-    })
-  }
-  finally {
-    isSubmitFormChangesLoading.value = false
-  }
-
-}
-
-async function saveNewOrder() {
-  try {
-    isSaveReorderLoading.value = true
-
-    const success = await CvLinksApi.reorder({
-      linksOrder: linksOrder.value,
-      profileId: selectedProfileId.value,
-    })
-
-    if (success) {
-      // Обновление ссылок для сброса состояния
-      await uploadLinks()
-    }
-  } catch (err) {
-    console.error(err)
-    toast.error('Произошла ошибка при изменении порядка ссылок', {
-      duration: 3000,
-      title: 'Ошибка',
-    })
-  }
-  finally {
-    isSaveReorderLoading.value = false
-  }
-}
-
-function moveLink(direction: 'up' | 'down') {
-  const selectedId = selectedLink.value?.id
-  if (!selectedId) return
-
-  const idx = links.value.findIndex((link) => link.id === selectedId)
-  // MOVE UP
-  if (direction === 'up') {
-    if (idx <= 0) {
-      return
-    }
-    ;[links.value[idx - 1], links.value[idx]] = [links.value[idx], links.value[idx - 1]]
-  }
-  // MOVE DOWNs
-  else if (direction === 'down') {
-    if (idx === -1 || idx === links.value.length - 1) {
-      return
-    }
-    ;[links.value[idx], links.value[idx + 1]] = [links.value[idx + 1], links.value[idx]]
-  }
-  for (let i = 0; i < links.value.length; i++) {
-    const link = links.value[i]
-
-    linksOrder.value[i] = {
-      id: link.id,
-      order: i + 1,
-      label: link.label,
-    }
-  }
-}
-
-function resetChangesOrder() {
-  linksOrder.value = links.value
-    .sort((a, b) => a.order - b.order)
-    .map((v) => ({
-      id: v.id,
-      order: v.order,
-      label: v.label
-    }))
-}
-
-function goToNewLinkPage() {
-  window.location.href = AppRoutes.admin.CvLinksNew
-}
-
-async function uploadLinks(options?: { resetSelection?: boolean }) {
-  // Если Нужно сбросить прошлое состояние формы перед рендерингом нового списка ссылок
-  if (options?.resetSelection) {
-    resetSelection()
-  }
-
-  links.value = await CvLinksApi.getListByProfileId(
-    selectedProfileId.value || _,
-  )
-  linksOrder.value = links.value.map((v) => ({ id: v.id, order: v.order, label: v.label }))
-}
-
-onBeforeMount(async () => {
-  await uploadLinks({ resetSelection: true })
-})
+  confirmUpdateField,
+  undoChanges,
+  submitFormChanges,
+  resetFormChanges,
+  saveNewOrder,
+  resetChangesOrder,
+  goToCreatePage: goToNewLinkPage,
+} = useCvLinksEditor()
 </script>
 
 <template>
